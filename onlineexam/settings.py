99861f7a3e1a8f5a130f405505c9f11f1e357ac4
udayabhanu97 <udayabhanus65@gmail.com>
2026-03-20 16:44:08 +0530

    disabled smtp email

--- onlineexam/settings.py
+++ onlineexam/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
